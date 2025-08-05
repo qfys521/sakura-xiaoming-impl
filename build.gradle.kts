8444c487cdaea07e38e831778d0964f2faf2b710
千枫·遇裳 <qfys521@morizero.com>
2025-08-05 16:57:35 +0800

    build: 🔨️add Kotlin serialization plugin to build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.2.0"
     id("com.gradleup.shadow") version "9.0.0-rc3"
     id("maven-publish")
+    kotlin("plugin.serialization") version "2.2.0"
 }
 
 group = "cn.qfys521"
