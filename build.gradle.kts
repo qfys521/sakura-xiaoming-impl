9e44815da333d9f10e67ab65de435f2e2c0cdc13
千枫·遇裳 <qfys521@morizero.com>
2025-08-05 16:06:56 +0800

    fix: 🪲️update shadow plugin version in project configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.0.0-rc3"
     id("maven-publish")
 }
 
